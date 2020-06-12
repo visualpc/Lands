b50bc8a87c291ca65f6756e2b9c60fa63ce8de3a
Pablo Cuello <pablocuelloc@hotmail.com>
2020-06-12 15:24:48 -0500

    First changes to GIT

---
+++ Lands/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Lands.Views;
+
+namespace Lands
+{
+    public partial class App : Application
+    {
+
+        #region Constructors
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new NavigationPage(new LoginPage());
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        } 
+        #endregion
+    }
+}
