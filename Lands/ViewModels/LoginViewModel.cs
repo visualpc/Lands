43df4dfe1373a40eb18b4afa42d4a8b0e56b7daf
Pablo Cuello <pablocuelloc@hotmail.com>
2020-06-12 20:22:30 -0500

    Edicion de BaseViewModel
    Edicion de BaseViewModel

--- Lands/ViewModels/LoginViewModel.cs
+++ Lands/ViewModels/LoginViewModel.cs
@@ -1,25 +1,79 @@
 ﻿namespace Lands.ViewModels
 {
-    
+    using GalaSoft.MvvmLight.Command;
     using System.Windows.Input;
+    using Xamarin.Forms;
 
-    public class LoginViewModel
+    public class LoginViewModel : BaseViewModel
     {
+
+        #region Attributes
+        private string email;
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
         #region Properties
         public string Email { get; set; }
-        public string Password { get; set; }
-        public bool IsRunning { get; set; }
+        public string Password
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
+
         public bool IsRemembered { get; set; }
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
         #endregion
 
         #region Commands
-        public ICommand LoginCommand { get; set; }
+        public ICommand LoginCommand 
+        {
+            get 
+            {
+                return new RelayCommand(Login);   
+            } 
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error", 
+                    "Your must enter an email", 
+                    "Accept");
+                return;
+            }
+
+
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Your must enter an password",
+                    "Accept");
+                return;
+            }
+        }
         #endregion
 
         #region Constructors
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.isEnabled = true;
         }
         #endregion
     }
